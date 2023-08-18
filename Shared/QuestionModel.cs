7b9a0180cb4ec9a85e85a4cf62c3f99ddf5875a0
Victor Wahlberg <wahlbergvictor01@gmail.com>
2023-08-18 15:03:10 +0200

    Fixed shit

--- Shared/QuestionModel.cs
+++ Shared/QuestionModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,6 +9,7 @@ namespace QuizaDjuret.Shared
 {
     public class QuestionModel
     {
+        [Key]
         public int QuestionId { get; set; }
         public int CorrectAnswerId { get; set; }
         public string Text { get; set; } = null!;
