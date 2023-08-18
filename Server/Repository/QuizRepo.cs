a91f7b21482b681de0a076574bb1f9ff27e50492
Victor Wahlberg <wahlbergvictor01@gmail.com>
2023-08-18 14:50:36 +0200

    Created Repository
    Created the QuizRepo

---
+++ Server/Repository/QuizRepo.cs
@@ -0,0 +1,14 @@
+﻿using QuizaDjuret.Server.Data;
+
+namespace QuizaDjuret.Server.Repository
+{
+    public class QuizRepo
+    {
+        private readonly AppDbContext context;
+
+        public QuizRepo(AppDbContext context)
+        {
+            this.context = context;
+        }
+    }
+}
