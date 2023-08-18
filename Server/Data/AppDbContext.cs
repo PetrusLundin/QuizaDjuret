6d956b5d636d1b66fbc03911700bac1a05c870e7
Victor Wahlberg <wahlbergvictor01@gmail.com>
2023-08-18 14:46:54 +0200

    Models n AppDbContext
    Created Models n AppDbContext for the project.

---
+++ Server/Data/AppDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using QuizaDjuret.Shared;
+
+namespace QuizaDjuret.Server.Data
+{
+    public class AppDbContext:DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext>options): base (options)
+        {
+            
+        }
+
+        public DbSet<UserModel> Highscore { get; set; }
+        public DbSet<AnswerModel> Answers { get; set; }
+        public DbSet<QuestionModel> Questions { get; set; }
+    }
+}
---
+++ Shared/AnswerModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuizaDjuret.Shared
+{
+    public class AnswerModel
+    {
+        [Key]
+        public int AnswerId { get; set; }
+        public int QuestionId { get; set; }
+        public QuestionModel Question { get; set; } = new();
+        public bool IsCorrect { get; set; }
+        public string Text { get; set; } = null!;
+
+    }
+}
