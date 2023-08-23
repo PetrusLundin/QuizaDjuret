53e92201e9e5633c625ac5747432b47d52134876
Petrus Lundin <petruslundin@live.com>
2023-08-23 11:37:08 +0200

    service controller
    communicating backend frontend.

---
+++ Client/Services/QuizService.cs
@@ -0,0 +1,27 @@
+﻿using Newtonsoft.Json;
+using System.Text.Json.Serialization;
+
+namespace QuizaDjuret.Client.Services
+{
+	public class QuizService
+	{
+		private readonly HttpClient httpClient;
+
+		public QuizService(HttpClient httpClient)
+        {
+			this.httpClient = httpClient;
+		}
+
+		public async Task<string> Testget(int id)
+		{
+			var response = await httpClient.GetAsync($"api/quiz/{id}");
+			if(response.IsSuccessStatusCode)
+			{
+				var json = await response.Content.ReadAsStringAsync();
+				//var quiz = JsonConvert.DeserializeObject<string>(json);
+				return json;
+			}
+			return null;
+		}
+    }
+}
