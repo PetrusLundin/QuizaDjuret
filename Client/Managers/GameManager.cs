c1762b97356b3612fe622ec79a444f9ebc946226
Minderg <105292841+Minderg@users.noreply.github.com>
2023-08-24 11:18:28 +0200

    GameManager
    Place to stash all the logic behind the game

---
+++ Client/Managers/GameManager.cs
@@ -0,0 +1,19 @@
+﻿namespace QuizaDjuret.Client.Managers
+{
+    public static class GameManager
+    {
+        public static int CurrentQuestion { get; set; } = 0;
+
+        public static bool isRunning { get; set; } = false;
+
+        public static void NextQuestion()
+        {
+            CurrentQuestion++;
+        }
+
+        //public static void StartGame()
+        //{
+
+        //}
+    }
+}
