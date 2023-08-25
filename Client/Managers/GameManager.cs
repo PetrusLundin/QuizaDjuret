d9073c13f953b98cc389f429ee06f94000a6c694
Noobdahl <michael.nordahl@hotmail.com>
2023-08-25 13:39:15 +0200

    Trying out architecture

--- Client/Managers/GameManager.cs
+++ Client/Managers/GameManager.cs
@@ -5,27 +5,31 @@ namespace QuizaDjuret.Client.Managers
     public static class GameManager
     {
         public static List<QuestionModel> Questions { get; set; } = new();
+        //TODO plocka questionmodel på currentquestion för att tracka i index sen?
         public static int CurrentQuestion { get; set; } = 0;
         public static string CurrentQuestionText { get; set; } = "";
+        public static string CurrentQuestionHint { get; set; } = "";
+        public static string CurrentQuestionImage { get; set; } = "";
+        public static string CurrentQuestionFunFact { get; set; } = "";
 
         public static bool isRunning { get; set; } = false;
 
         public static void NextQuestion()
         {
-            if (CurrentQuestion <  Questions.Count - 1)
+            if (CurrentQuestion < Questions.Count)
             {
+                //TODO plocka rätt fråga ur array baserat på svårighetsgrad 1-10, 11-20 osv
                 CurrentQuestion++;
-                CurrentQuestionText = Questions[CurrentQuestion].Text;
+                CurrentQuestionText = Questions[CurrentQuestion - 1].Text;
+                CurrentQuestionHint = Questions[CurrentQuestion - 1].Hint;
+                CurrentQuestionFunFact = Questions[CurrentQuestion - 1].FunFact;
+                CurrentQuestionImage = Questions[CurrentQuestion - 1].ImageURL;
             }
             else
             {
                 isRunning = false;
                 CurrentQuestionText = "No more Questions";
             }
-            //TODO plocka rätt fråga ur array
-            //ändra svaren
-            //ändra hint
-            //ändra bild
         }
 
 
