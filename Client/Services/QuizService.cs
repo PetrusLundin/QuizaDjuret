9f4307bf53831148023c6e96606540c9cdc3bf5b
Noobdahl <michael.nordahl@hotmail.com>
2023-08-29 10:11:22 +0200

    Question loading in manager
    Changed how we load a question, now currentquestion is available as an object.

--- Client/Services/QuizService.cs
+++ Client/Services/QuizService.cs
@@ -3,39 +3,39 @@ using QuizaDjuret.Shared;
 
 namespace QuizaDjuret.Client.Services
 {
-    public class QuizService
-    {
-        private readonly HttpClient httpClient;
+	public class QuizService
+	{
+		private readonly HttpClient httpClient;
 
-        public QuizService(HttpClient httpClient)
-        {
-            this.httpClient = httpClient;
-        }
+		public QuizService(HttpClient httpClient)
+		{
+			this.httpClient = httpClient;
+		}
 
-        public async Task<List<QuestionModel>> GetAllAsync()
-        {
-            var response = await httpClient.GetAsync($"api/quiz");
-            if (response.IsSuccessStatusCode)
-            {
-                var json = await response.Content.ReadAsStringAsync();
-                var quiz = JsonConvert.DeserializeObject<List<QuestionModel>>(json);
-                return quiz;
-            }
-            return null;
-        }
+		public async Task<List<QuestionModel>> GetAllAsync()
+		{
+			var response = await httpClient.GetAsync($"api/quiz");
+			if (response.IsSuccessStatusCode)
+			{
+				var json = await response.Content.ReadAsStringAsync();
+				var quiz = JsonConvert.DeserializeObject<List<QuestionModel>>(json);
+				return quiz;
+			}
+			return null;
+		}
 
-        public async Task<List<AnswerModel>> GetAnswers(int questionId)
-        {
-            var answerResponse = await httpClient.GetAsync($"api/answer/{questionId}");
-            if (answerResponse.IsSuccessStatusCode)
-            {
-                var answerjson = await answerResponse.Content.ReadAsStringAsync();
-                var answer = JsonConvert.DeserializeObject<List<AnswerModel>>(answerjson);
-                return answer;
+		//public async Task<List<AnswerModel>> GetAnswers(int questionId)
+		//{
+		//    var answerResponse = await httpClient.GetAsync($"api/answer/{questionId}");
+		//    if (answerResponse.IsSuccessStatusCode)
+		//    {
+		//        var answerjson = await answerResponse.Content.ReadAsStringAsync();
+		//        var answer = JsonConvert.DeserializeObject<List<AnswerModel>>(answerjson);
+		//        return answer;
 
-            }
+		//    }
 
-            return null;
-        }
-    }
+		//    return null;
+		//}
+	}
 }
