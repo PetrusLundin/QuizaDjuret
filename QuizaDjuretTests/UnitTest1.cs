7f530dbf04a3e6daa8b049170e1abd0965555ad9
Minderg <105292841+Minderg@users.noreply.github.com>
2023-09-04 10:28:10 +0200

    SecondTest

--- QuizaDjuretTests/UnitTest1.cs
+++ QuizaDjuretTests/UnitTest1.cs
@@ -17,12 +17,28 @@ namespace QuizaDjuretTests
         [Fact]
 		public void NextQuestion_Test()
 		{
+			// Arrange
 			GameManager.CurrentQuestionNumber = 0;
 
-
+			// Act
 			GameManager.NextQuestion();
 
+			// Assert
 			Assert.Equal(1, GameManager.CurrentQuestionNumber);
 		}
+
+		[Fact]
+		public void ResetQuiz_Test()
+		{
+			// Arrange
+			GameManager.CurrentQuestionNumber = 69;
+
+			// Act
+			GameManager.ResetQuiz();
+
+			// Assert
+			Assert.Equal(0, GameManager.CurrentQuestionNumber);
+		}
+
 	}
 }
