b62ce68db8dee1fad312bee1a5418e0a7b14917b
Noobdahl <michael.nordahl@hotmail.com>
2023-09-07 17:36:20 +0200

    Save Score to dB

---
+++ Client/Services/ScoreService.cs
@@ -0,0 +1,32 @@
+﻿using QuizaDjuret.Client.Managers;
+using QuizaDjuret.Shared;
+using System.Net.Http.Json;
+
+namespace QuizaDjuret.Client.Services
+{
+	public class ScoreService
+	{
+		private readonly HttpClient httpClient;
+
+		public ScoreService(HttpClient httpClient)
+		{
+			this.httpClient = httpClient;
+		}
+
+		public async Task<bool> AddUserScore(string playerName)
+		{
+			UserModel user = new()
+			{
+				Name = playerName,
+				Score = ScoreManager.Score
+			};
+			var response = await httpClient.PostAsJsonAsync<UserModel>("api/score", user);
+
+			if (response.IsSuccessStatusCode)
+			{
+				return true;
+			}
+			return false;
+		}
+	}
+}
---
+++ Server/Repository/ScoreRepo.cs
@@ -0,0 +1,23 @@
+﻿using QuizaDjuret.Server.Data;
+using QuizaDjuret.Shared;
+
+namespace QuizaDjuret.Server.Repository
+{
+	public class ScoreRepo
+	{
+		private readonly AppDbContext context;
+
+		public ScoreRepo(AppDbContext context)
+		{
+			this.context = context;
+		}
+
+		public async Task<bool> AddUserAsync(UserModel userToAdd)
+		{
+			context.Highscore.Add(userToAdd);
+			await context.SaveChangesAsync();
+
+			return true;
+		}
+	}
+}
