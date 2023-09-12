4b5d4952d48aaaff8e3beb2e654a2c60580ac3ce
Minderg <105292841+Minderg@users.noreply.github.com>
2023-09-12 17:53:31 +0200

    testing

--- Client/wwwroot/scripts.js
+++ Client/wwwroot/scripts.js
@@ -2,10 +2,17 @@ window.quizFunctions = {
     changeButtonText: function (element) {
         element.classList.add("red-text");
     },
+    correctAnswerText: function (element) {
+        element.classList.add("green-text");
+    },
     resetButtonText: function () {
         var eles = document.getElementsByClassName('red-text');
         Array.from(eles).forEach(function (element) {
             element.classList.remove('red-text');
+        });
+        var eles = document.getElementsByClassName('green-text');
+        Array.from(eles).forEach(function (element) {
+            element.classList.remove('green-text');
         });
     },
     removeSparaKnapp: function () {
